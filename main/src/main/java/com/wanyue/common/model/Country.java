a5cd42a26c39c7e65ef715992a036e0b509477b3
ngon nguyen <nvngon2604@gmail.com>
2025-04-25 13:27:07 +0700

    Other regions country

---
+++ main/src/main/java/com/wanyue/common/model/Country.java
@@ -0,0 +1,46 @@
+package com.wanyue.common.model;
+
+import java.util.regex.Pattern;
+
+public class Country {
+    private String name;
+    private String code;
+    private String dialCode;
+    private int flagResId;
+    private String phoneRegex;
+
+    public Country(String name, String code, String dialCode, int flagResId, String phoneRegex) {
+        this.name = name;
+        this.code = code;
+        this.dialCode = dialCode;
+        this.flagResId = flagResId;
+        this.phoneRegex = phoneRegex;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public String getDialCode() {
+        return dialCode;
+    }
+
+    public int getFlagResId() {
+        return flagResId;
+    }
+
+    public String getPhoneRegex() {
+        return phoneRegex;
+    }
+
+    public boolean isValidPhoneNumber(String phoneNumber) {
+        if (phoneNumber == null || phoneNumber.isEmpty()) {
+            return false;
+        }
+        return Pattern.matches(phoneRegex, phoneNumber);
+    }
+}
--- main/src/main/java/com/wanyue/main/view/activity/LoginActivity.java
+++ main/src/main/java/com/wanyue/main/view/activity/LoginActivity.java
@@ -1,342 +1,179 @@
 package com.wanyue.main.view.activity;
 
-import android.app.Activity;
-import android.app.Dialog;
-import android.content.Intent;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextUtils;
+import android.text.TextWatcher;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
-import androidx.annotation.Nullable;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
+import androidx.appcompat.app.AppCompatActivity;
 
-import com.alibaba.android.arouter.facade.annotation.Route;
-import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.JSONObject;
-import com.wanyue.common.CommonAppConfig;
-import com.wanyue.common.Constants;
-import com.wanyue.common.HtmlConfig;
-import com.wanyue.common.activity.BaseActivity;
-import com.wanyue.common.activity.WebViewActivity;
-import com.wanyue.common.bean.ConfigBean;
-import com.wanyue.common.bean.DataListner;
-import com.wanyue.common.bean.UserBean;
-import com.wanyue.common.business.TimeModel;
-import com.wanyue.common.business.acmannger.ActivityMannger;
-import com.wanyue.common.http.BaseHttpCallBack;
-import com.wanyue.common.http.HttpCallback;
-import com.wanyue.common.http.ParseHttpCallback;
-import com.wanyue.common.interfaces.OnItemClickListener;
-import com.wanyue.common.mob.LoginData;
-import com.wanyue.common.mob.MobBean;
-import com.wanyue.common.mob.MobCallback;
-import com.wanyue.common.mob.MobLoginUtil;
-import com.wanyue.common.utils.DialogUitl;
-import com.wanyue.common.utils.ListUtil;
-import com.wanyue.common.utils.RouteUtil;
-import com.wanyue.common.utils.SpUtil;
-import com.wanyue.common.utils.ToastUtil;
-import com.wanyue.common.utils.ValidatePhoneUtil;
-import com.wanyue.common.utils.WordUtil;
-import com.wanyue.live.activity.LiveAnchorActivity;
+import com.wanyue.common.dialog.CountryPickerDialog;
+import com.wanyue.common.model.Country;
+import com.wanyue.common.utils.CountryUtils;
 import com.wanyue.main.R;
-import com.wanyue.main.R2;
-import com.wanyue.main.adapter.LoginTypeAdapter;
-import com.wanyue.main.api.MainAPI;
-import com.wanyue.main.bean.LoginCommitBean;
-import java.util.List;
-import butterknife.BindView;
-import butterknife.OnClick;
-import butterknife.OnTextChanged;
+import com.wanyue.main.view.activity.MainActivity;
 
-@Route(path = RouteUtil.PATH_LOGIN)
-public class LoginActivity extends BaseActivity implements TimeModel.TimeListner, OnItemClickListener<MobBean> {
+import android.content.Intent;
 
-    @BindView(R2.id.tv_phone)
-    EditText mTvPhone;
-    @BindView(R2.id.tv_code)
-    EditText mTvCode;
-    @BindView(R2.id.btn_get_code)
-    TextView mBtnGetCode;
-    @BindView(R2.id.btn_login)
-    Button mBtnLogin;
-    @BindView(R2.id.recyclerView)
-    RecyclerView mRecyclerView;
-    @BindView(R2.id.btn_tip)
-    TextView mBtnTip;
-    @BindView(R2.id.tip_group)
-    LinearLayout mTipGroup;
-    @BindView(R2.id.img_launcher)
-    ImageView mImgLauncher;
+public class LoginActivity extends AppCompatActivity {
 
-    private TimeModel mTimeModel;
-    private LoginCommitBean mLoginCommitBean;
-    private MobLoginUtil mLoginUtil;
-
+    private LinearLayout countryPickerContainer;
+    private ImageView imgCountryFlag;
+    private TextView tvCountryCode;
+    private EditText tvPhone;
+    private EditText tvCode;
+    private Button btnLogin;
+    private TextView btnGetCode;
+    
+    private Country selectedCountry;
 
     @Override
-    public void init() {
-        mBtnTip.setText(getString(R.string.login_tip_2));
-        initCommitData();
-        initThirdData();
-        mImgLauncher.setImageResource(CommonAppConfig.getAppIconRes());
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        
+        initView();
+        initData();
+        initListener();
     }
-
-    private void initCommitData() {
-        mLoginCommitBean=new LoginCommitBean();
-        mLoginCommitBean.setDataListner(new DataListner() {
+    
+    private void initView() {
+        countryPickerContainer = findViewById(R.id.country_picker_container);
+        imgCountryFlag = findViewById(R.id.img_country_flag);
+        tvCountryCode = findViewById(R.id.tv_country_code);
+        tvPhone = findViewById(R.id.tv_phone);
+        tvCode = findViewById(R.id.tv_code);
+        btnLogin = findViewById(R.id.btn_login);
+        btnGetCode = findViewById(R.id.btn_get_code);
+    }
+    
+    private void initData() {
+        selectedCountry = CountryUtils.getCountryByCode(this, "CN");
+        updateCountryUI(selectedCountry);
+    }
+    
+    private void initListener() {
+        countryPickerContainer.setOnClickListener(v -> showCountryPickerDialog());
+        
+        tvPhone.addTextChangedListener(new TextWatcher() {
             @Override
-            public void compelete(boolean isCompelete) {
-                mBtnLogin.setEnabled(isCompelete);
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                validatePhoneNumber();
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {}
+        });
+        
+        tvCode.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                validateForm();
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {}
+        });
+        
+        btnGetCode.setOnClickListener(v -> {
+            if (validatePhoneNumber()) {
+                requestVerificationCode();
+            }
+        });
+        
+        btnLogin.setOnClickListener(v -> {
+            if (validateForm()) {
+                login();
             }
         });
     }
-
-    /*初始化第三方登录的数据*/
-    private void initThirdData() {
-        List<MobBean> list = MobBean.getLoginTypeList(Constants.MOB_WX);
-        if (ListUtil.haveData(list)) {
-            mRecyclerView = findViewById(R.id.recyclerView);
-            mRecyclerView.setHasFixedSize(true);
-            mRecyclerView.setLayoutManager(new LinearLayoutManager(mContext, LinearLayoutManager.HORIZONTAL, false));
-            LoginTypeAdapter adapter = new LoginTypeAdapter(mContext, list);
-            adapter.setOnItemClickListener(this);
-            mRecyclerView.setAdapter(adapter);
-            mLoginUtil = new MobLoginUtil();
-        }
-    }
-
-    /*监听手机号输入框信息*/
-    @OnTextChanged(value = R2.id.tv_phone, callback = OnTextChanged.Callback.TEXT_CHANGED)
-    public void watchPhoneTextChange(CharSequence sequence, int start, int before, int count){
-        String phoneString=sequence.toString();
-        mLoginCommitBean.setPhoneString(phoneString);
-        mBtnGetCode.setEnabled(ValidatePhoneUtil.validateMobileNumber(phoneString));
-    }
-
-    /*监听验证码号输入框信息*/
-    @OnTextChanged(value = R2.id.tv_code, callback = OnTextChanged.Callback.TEXT_CHANGED)
-    public void watchCodeTextChange(CharSequence sequence, int start, int before, int count){
-        String codeString=sequence.toString();
-        mLoginCommitBean.setCheckString(codeString);
-    }
-    /*获取验证码*/
-    @OnClick(R2.id.btn_get_code)
-    public void getCode() {
-       final String phoneNum=mTvPhone.getText().toString();
-        if(!ValidatePhoneUtil.validateMobileNumber(phoneNum)){
-            mTvPhone.setError(WordUtil.getString(R.string.login_phone_error));
-            mTvPhone.requestFocus();
-            return;
-        }
-        MainAPI.getVerifyKey(new ParseHttpCallback<JSONObject>() {
-            @Override
-            public void onSuccess(int code, String msg, JSONObject info) {
-                if(info!=null){
-                 String infoString= info.getString("key");
-                 getVerifyCode(phoneNum,infoString);
-                }else{
-                    ToastUtil.show(msg);
-                }
-            }
-            @Override
-            public void onError(Throwable e) {
-                if (e != null) {
-                    ToastUtil.show(e.getMessage());
-                }
-            }
+    
+    private void showCountryPickerDialog() {
+        CountryPickerDialog dialog = new CountryPickerDialog(this, country -> {
+            selectedCountry = country;
+            updateCountryUI(country);
+            validatePhoneNumber();
         });
-    }
-
-    private void getVerifyCode(String phoneNum,String infoString) {
-        MainAPI.getVerifyCode(phoneNum, "login", infoString, new HttpCallback() {
-            @Override
-            public void onSuccess(int code, String msg, String[] info) {
-                ToastUtil.show(msg);
-                if(isSuccess(code)){
-                    getLoginCodeSucc();
-                }
-            }
-        });
-    }
-
-    private void getLoginCodeSucc() {
-        initTimeModel();
-        mTimeModel.start();
-        mBtnGetCode.setEnabled(false);
-    }
-
-    @OnClick(R2.id.btn_register)
-    public void toRegister(){
-        startActivityForResult(RegisterActivity.class,RegisterActivity.REGISTER);
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if(requestCode==RegisterActivity.REGISTER&&resultCode==RESULT_OK){
-            String phone=data.getStringExtra(Constants.DATA);
-            mTvPhone.setText(phone);
-        }
-    }
-
-    @OnClick(R2.id.btn_login)
-    public void login() {
-        if(mLoginCommitBean==null){
-            return;
-        }
-        String phoneString=mLoginCommitBean.getPhoneString();
-        String codeString=mLoginCommitBean.getCheckString();
-
-        MainAPI.loginByCode(phoneString,codeString, mParseHttpCallback);
-    }
-
-    private ParseHttpCallback mParseHttpCallback= new ParseHttpCallback<JSONObject>() {
-        @Override
-        public void onSuccess(int code, String msg, JSONObject info) {
-            if(isSuccess(code)){
-                ToastUtil.show(com.wanyue.common.R.string.login_auth_success);
-                loginingSucc(info);
-            }else{
-                ToastUtil.show(msg);
-            }
-        }
-        @Override
-        public void onError(Throwable e) {
-            if (e != null) {
-                ToastUtil.show(e.getMessage());
-            }
-        }
-    };
-
-    private void loginingSucc(JSONObject jsonObject) {
-        String token = jsonObject.getString("token");
-        CommonAppConfig.setLoginInfo( "0",token, true);
-        String sign = jsonObject.getString("usersig");
-        SpUtil.getInstance().setStringValue(SpUtil.TX_IM_USER_SIGN,sign);
-        askUserInfo();
-    }
-
-    private void askUserInfo() {
-        MainAPI.getBaseInfo(new HttpCallback() {
-            @Override
-            public void onSuccess(int code, String msg, String[] info) {
-                if(BaseHttpCallBack.isSuccess(code)&&info.length>0){
-                    String json=info[0];
-                    UserBean userBean=JSON.parseObject(json,UserBean.class);
-                    CommonAppConfig.setUserBean(userBean,json);
-                    Activity activity= ActivityMannger.getInstance().getBaseActivity();
-                    if(activity==null){
-                       startActivity(MainActivity.class);
-                    }
-                    finish();
-                }
-            }
-        });
-    }
-
-    private void initTimeModel() {
-        if(mTimeModel==null){
-            mTimeModel=new TimeModel()
-                    .setTotalUseTime(60)
-                    .setState(TimeModel.COUNT_DOWN)
-                    .setAfterString("s后重新获取");
-            mTimeModel.addTimeListner(this);
-        }
-    }
-
-    @Override
-    public int getLayoutId() {
-        return R.layout.activity_login;
-    }
-
-
-    @Override
-    public void onDestroy() {
-        super.onDestroy();
-        if(mTimeModel!=null){
-           mTimeModel.clear();
-        }
-    }
-
-
-    private long mLastClickBackTime;
-    @Override
-    public void onBackPressed() {
-        long curTime = System.currentTimeMillis();
-        if (curTime - mLastClickBackTime > 2000) {
-            mLastClickBackTime = curTime;
-            ToastUtil.show(R.string.main_click_next_exit);
-            return;
-        }
-        ActivityMannger.getInstance().clearAllActivity();
-    }
-
-    @Override
-    protected boolean shouldBindButterKinfe() {
-        return true;
-    }
-
-    @Override
-    public void time(String string) {
-        mBtnGetCode.setText(string);
-    }
-
-    @Override
-    public void compelete() {
-        mBtnGetCode.setEnabled(true);
-        mBtnGetCode.setText(R.string.login_get_code);
-    }
-
-    /*第三方登陆*/
-    @Override
-    public void onItemClick(final MobBean bean, int position) {
-        if (mLoginUtil == null) {
-            return;
-        }
-        final Dialog dialog = DialogUitl.loginAuthDialog(mContext);
         dialog.show();
-        mLoginUtil.execute(bean.getType(), new MobCallback() {
-            @Override
-            public void onSuccess(Object data) {
-                if (data != null) {
-                    loginByThird(bean.getType(),(LoginData) data);
-                }
-            }
-            @Override
-            public void onError() {
-                disMissLoadingDialog(dialog);
-
-            }
-            @Override
-            public void onCancel() {
-                disMissLoadingDialog(dialog);
-            }
-            @Override
-            public void onFinish() {
-                disMissLoadingDialog(dialog);
-            }
-        });
     }
-
-
-
-    private void disMissLoadingDialog(Dialog dialog) {
-        if (dialog != null&&dialog.isShowing()) {
-            dialog.dismiss();
-            dialog=null;
+    
+    private void updateCountryUI(Country country) {
+        if (country != null) {
+            imgCountryFlag.setImageResource(country.getFlagResId());
+            tvCountryCode.setText(country.getDialCode());
         }
     }
-    /*点击用户协议*/
-    @OnClick(R2.id.btn_tip)
-    public void clickTip() {
-        WebViewActivity.forward(this, HtmlConfig.LOGIN_PRIVCAY,false);
+    
+    private boolean validatePhoneNumber() {
+        String phoneNumber = tvPhone.getText().toString().trim();
+        boolean isValid = false;
+        
+        if (!TextUtils.isEmpty(phoneNumber) && selectedCountry != null) {
+            isValid = selectedCountry.isValidPhoneNumber(phoneNumber);
+        }
+        
+        btnGetCode.setEnabled(isValid);
+        
+        if (!TextUtils.isEmpty(phoneNumber) && !isValid) {
+            tvPhone.setError("Invalid phone number for country " + selectedCountry.getName());
+        } else {
+            tvPhone.setError(null);
+        }
+        
+        validateForm();
+        return isValid;
     }
-
-    private void loginByThird(String loginType,LoginData data) {
-        MainAPI.loginByThird(data,mParseHttpCallback);
+    
+    private boolean validateForm() {
+        String phoneNumber = tvPhone.getText().toString().trim();
+        String code = tvCode.getText().toString().trim();
+        
+        boolean isPhoneValid = !TextUtils.isEmpty(phoneNumber) && selectedCountry != null && 
+                               selectedCountry.isValidPhoneNumber(phoneNumber);
+        boolean isCodeValid = !TextUtils.isEmpty(code) && code.length() >= 4;
+        
+        btnLogin.setEnabled(isPhoneValid && isCodeValid);
+        
+        return isPhoneValid && isCodeValid;
+    }
+    
+    private void requestVerificationCode() {
+        // TODO: Gửi yêu cầu mã xác nhận đến số điện thoại
+        // Đây là nơi bạn sẽ gọi API để gửi SMS chứa mã xác nhận
+        
+        // Giả lập đếm ngược thời gian chờ
+        btnGetCode.setEnabled(false);
+        btnGetCode.setText("60s");
+        
+        Toast.makeText(this, "A confirmation code has been sent to your phone number", Toast.LENGTH_SHORT).show();
+    }
+    
+    private void login() {
+        String phoneNumber = tvPhone.getText().toString().trim();
+        String code = tvCode.getText().toString().trim();
+        
+        Toast.makeText(this, "Signing in...", Toast.LENGTH_SHORT).show();
+        
+        // Sửa lại cách khai báo Intent với package name đầy đủ
+        Intent intent = new Intent();
+        intent.setClassName(this, "com.wanyue.main.view.activity.MainActivity");
+        // Hoặc nếu MainActivity nằm trong package khác, thay đổi package name tương ứng
+        
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+        finish();
     }
 }
