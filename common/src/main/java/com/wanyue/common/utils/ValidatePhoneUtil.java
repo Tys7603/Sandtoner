65286f876219b091380f2f4fe73fe9eb7de0d673
ngon nguyen <nvngon2604@gmail.com>
2025-05-11 17:14:50 +0700

    validate phone for south africa

--- common/src/main/java/com/wanyue/common/utils/ValidatePhoneUtil.java
+++ common/src/main/java/com/wanyue/common/utils/ValidatePhoneUtil.java
@@ -7,14 +7,20 @@ import java.util.regex.Pattern;
 
 
 public class ValidatePhoneUtil {
-    //判断手机号码的正则表达式
-    private static final String MOBILE_NUM_REGEX = "^((13[0-9])|(14[0-9])|(15[^4,\\D])|(18[0-9])|(17[0-9])|(16[0-9])|(19[0-9]))\\d{8}$";
 
+    //判断手机号码的正则表达式
+    //private static final String MOBILE_NUM_REGEX = "^((13[0-9])|(14[0-9])|(15[^4,\\D])|(18[0-9])|(17[0-9])|(16[0-9])|(19[0-9]))\\d{8}$";
+
+    // South African phone number regex (accepts +27, 27, or 0)
+    private static final String MOBILE_NUM_REGEX = "^(\\+?27[6-8][0-9]{8}|0[6-8][0-9]{8})$";
 
     /**
-     * 验证一个号码是不是手机号，以前这个地方是用正则判断的，现在改由服务端验证
+     * Validate South African phone number
+     * Format: +27 or 0 followed by 9 digits
+     * Total length: 10-11 digits (excluding country code)
      *
      * @param mobileNumber
+     * @return true if valid South African phone number
      */
     public static boolean validateMobileNumber(String mobileNumber) {
         if(TextUtils.isEmpty(mobileNumber)){
